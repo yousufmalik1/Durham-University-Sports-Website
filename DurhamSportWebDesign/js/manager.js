efd0d6fc6a67261d8e1cc3724a0be69a262c8051
xuantong <guoxuantong1223@gmail.com>
2019-05-09 19:43:02 +0100

    web design 1st version by xuantong
    Here is the first version of our web structure, you can program based on it.

---
+++ DurhamSportWebDesign/js/manager.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    //mobile menu toggling
+    $("#menu_icon").click(function(){
+        $("header nav ul").toggleClass("show_menu");
+        $("#menu_icon").toggleClass("close_menu");
+        return false;
+    });
+
+    $("a").mouseout(function(){
+        var tooltip = $(".tooltip");
+        tooltip.remove();
+    });
+
+});
+
+
+
+
+
