998dd3db8f554ced3a54b12f3c4be2bc4aa0f86a
yousufmalik1 <50234385+yousufmalik1@users.noreply.github.com>
2019-05-17 13:21:26 +0100

    Update admindeletebooking.php

--- DurhamSportWebDesign/php/admindeletebooking.php
+++ DurhamSportWebDesign/php/admindeletebooking.php
@@ -7,7 +7,7 @@ header("location:index.php");
 }*/
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=XDurhamSports', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=xdurhamsports', 'root', '');
     
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
