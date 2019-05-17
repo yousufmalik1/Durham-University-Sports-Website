d8a9ab74e0cbc6096fa178b340105d8a7c6f2a2e
PhilipWright96 <49524546+PhilipWright96@users.noreply.github.com>
2019-05-17 11:52:04 +0100

    Load file (Required for calendar.php)

---
+++ DurhamSportWebDesign/php/load.php
@@ -0,0 +1,28 @@
+<?php
+
+//load.php
+
+
+$data = array();
+
+$pdo = new PDO('mysql:host=mysql.dur.ac.uk;dbname=Iqjhq34_mock-facilities', 'qjhq34', 'memory84');
+$statement = $pdo->query("SELECT booking.bookingID, booking.bookingTitle, booking.start, booking.end, facility.facilityName
+FROM booking
+INNER JOIN facility ON booking.facilityID = facility.facilityID");
+
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($result as $row)
+{
+ $data[] = array(
+  'id'   => $row["bookingID"],
+  'title'   => $row["facilityName"].": ".$row["bookingTitle"],
+  'start'   => $row["start"],
+  'end'   => $row["end"]
+
+ );
+}
+
+echo json_encode($data);
+
+?>
