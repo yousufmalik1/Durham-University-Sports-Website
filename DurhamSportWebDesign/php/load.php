4c90d69d4bcbce3b8e84dd680150a71f0afefb93
PhilipWright96 <49524546+PhilipWright96@users.noreply.github.com>
2019-05-21 00:10:40 +0100

    Update load.php

--- DurhamSportWebDesign/php/load.php
+++ DurhamSportWebDesign/php/load.php
@@ -7,9 +7,10 @@ $data = array();
 
 //$pdo = new PDO('mysql:host=mysql.dur.ac.uk;dbname=Iqjhq34_mock-facilities', 'qjhq34', 'memory84');
 $pdo = new PDO('mysql:host=localhost;dbname=xdurhamsports', 'root', '');
-$statement = $pdo->query("SELECT booking.bookingID, booking.eventID, booking.bookingTitle, booking.bookingDate booking.startTime, booking.endTime, facility.facilityName
+$statement = $pdo->query("SELECT booking.bookingID, booking.eventID, booking.bookingTitle, booking.bookingDate booking.startTime, booking.endTime, facility.facilityName,event.eventName
 FROM booking
-INNER JOIN facility ON booking.facilityID = facility.facilityID");
+INNER JOIN facility ON booking.facilityID = facility.facilityID
+INNER JOIN event ON booking.eventID = event.eventID");
 
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
@@ -31,7 +32,8 @@ if ($row["eventID"] == 3001){
  $data[] = array(
   'id'   => $row["bookingID"],
   'color' => $color,
-  'title'   => $row["facilityName"].": ".$row["bookingTitle"],
+  'event' => $row["eventName"],
+  'title'   => $row["bookingTitle"],
   'start'   => $row["bookingDate"]." ".$row["startTime"],
   'end'   => $row["bookingDate"]." ".$row["endTime"],
   'facility' => $row["facilityName"]
