f5b5243c1f53e0220448ce6f613aee6dce054de6
yousufmalik1 <50234385+yousufmalik1@users.noreply.github.com>
2019-05-21 16:43:44 +0100

    Update adminDeleteFacility.php

--- DurhamSportWebDesign/php/adminDeleteFacility.php
+++ DurhamSportWebDesign/php/adminDeleteFacility.php
@@ -1,23 +1,8 @@
 <?php
 session_start();
 require 'database.php';
-/*/if($_SESSION['username'] !== 'Admin')
-{
-header("location:index.php");
-}*/
+$pdo = make_database_connection();
 
-try{
-    $pdo = new PDO('mysql:host=localhost;dbname=xdurhamsports', 'root', '');
-    
-    // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    //echo "Connected successfully";
-}   
-        catch(PDOException $e)
-        {
-        echo "Connection failed: " . $e->getMessage();
-        }
 
 $del_id = $_GET['del_id'];
 
