16419d168d44bc930ceffea04feb274bd81b44b9
CHENGLONG ZHENG <50271694+Corwin00@users.noreply.github.com>
2019-05-21 07:19:01 +0100

    debug

--- DurhamSportWebDesign/php/booking-confirm.php
+++ DurhamSportWebDesign/php/booking-confirm.php
@@ -10,34 +10,23 @@ header("Content-Type: application/json;charset=utf-8");
 require('database.php');
 require_once("./functions.php");
 $pdo = make_database_connection();
-//Form2
-$bookingTitle=$_POST["bookingtitle"];
-$facilityName=$_POST["facilityName"];
-$bookingDate=$_POST["date"];
-$startTime=$_POST["timefrom"];
-$endTime=$_POST["timeto"];
-$people=$_POST["people"];
-$notes=$_POST["notes"];
-$eventName=$_POST["eventName"];
+
+$bookingtitle = $_POST['bookingtitle'];
+$facilityId = $_POST['facilityId'];
+$bookingDate = $_POST['date'];
+$startTime = $_POST['time'].":00:00";
+$endTime = ($_POST['time']+1).":00:00";
+
 $username=$_SESSION['username'];
 
-//facilityID
-$sql1 = "select * from facility where facilityName = '$facilityName'";
-$result1 = $pdo->query($sql1);
-$row1 = $result1->fetch(PDO::FETCH_ASSOC);
-$facilityID = $row1['facilityID'];
 //userID
 $sql2 = "select * from user where username = '$username'";
 $result2 = $pdo->query($sql2);
 $row2 = $result2->fetch(PDO::FETCH_ASSOC);
 $userID = $row2['userID'];
 $email = $row2['email'];
-//eventID
-$result3 = $pdo->query("select eventID from event where eventName = '$eventName'");
-$row3 = $result3->fetch(PDO::FETCH_ASSOC);
-$eventID = $row3['eventID'];
 //Insert booking
-$sql4 = "INSERT INTO `booking`(`userID`, `facilityID`, `eventID`, `bookingDate`, `startTime`, `endTime`, `people`, `bookingTitle`, `notes`) VALUES ('$userID','$facilityID','$eventID','$bookingDate','$startTime','$endTime','$people','$bookingTitle','$notes')";
+$sql4 = "INSERT INTO `booking`(`userID`, `facilityID`, `bookingDate`, `startTime`, `endTime`, `bookingTitle`) VALUES ('$userID','$facilityId','$bookingDate','$startTime','$endTime','$bookingtitle')";
 $result4 = $pdo->query($sql4);
 if($pdo->lastInsertId()!=null){
     echo "true";
@@ -46,11 +35,6 @@ if($pdo->lastInsertId()!=null){
 }
 $pdo = null;
 //Send email
-$flag = sendMail($email,'Booking Confirmation for DUS-Team 1','Dear, Your booking has been confirmed.');
-if($flag){
-    header ("Location:../html/pages-confirm-mail.html");
-}else{
-    echo "Send email failure！";
-}
+sendMail($email,'Booking Confirmation for DUS-Team 1','Dear, Your booking has been confirmed.');
 
 ?>
