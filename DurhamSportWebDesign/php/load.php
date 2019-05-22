1c488ca307b5ca2dea326076dae0190458d6bf1b
PhilipWright96 <49524546+PhilipWright96@users.noreply.github.com>
2019-05-22 21:05:07 +0100

    Updated to support block bookings

--- DurhamSportWebDesign/php/load.php
+++ DurhamSportWebDesign/php/load.php
@@ -42,10 +42,10 @@ if ($row["eventID"] == 3001){
 
 //Second SQL selection
 
-$statement = $pdo->query("SELECT block_booking.eventID, block_booking.eventName, block_booking.dateFrom, block_booking.dateTo, block_booking.timeFrom, block_booking.timeTo, facility.facilityName
+$statement = $pdo->query("SELECT block_booking.eventID, block_booking.eventName, block_booking.dateFrom, block_booking.dateTo, block_booking.timeFrom, block_booking.timeTo, block_booking.dow, facility.facilityName
 FROM block_booking
 INNER JOIN facility ON block_booking.facilityID = facility.facilityID");
-
+ 
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 $color = '#47e5f7';
@@ -53,28 +53,63 @@ $color = '#47e5f7';
 foreach($result as $row)
 {
     
-if ($row["eventName"] == "Exams"){
-     $color = '#f46b6b';
+    if ($row["eventName"] == "Exams"){
+         $color = '#f46b6b';
+
+     }
+
+    //If its a examination
+    if ($row["eventName"] == "Exercise Class"){
+         $color = '#63f9b1';
+     }
+
+    if ($row["eventName"] == "Non-Exercise Class"){
+        $color = '#f5ff72';
+    }
+
+    //Get the dow
+    $dow = $row["dow"];
+    $dow_array = array_map('intval', str_split($dow));
+    
+    $begin_str = $row["dateFrom"]." ". $row["timeFrom"];
+    $end_str = $row["dateTo"]." ". $row["timeTo"];
+    
+    $begin_date = new DateTime($begin_str);
+    $end_date = new DateTime($end_str);
+    
+    $interval = DateInterval::createFromDateString('1 day');
+    $period = new DatePeriod($begin_date, $interval, $end_date);
+    
+    foreach ($period as $dt) {
+        
+        $check_date = $dt->format("Y-m-d");
+        $day_num = $dt->format("N");
+        
+
+        if (in_array($day_num, $dow_array)){
+            $data[] = array(
+              'id'   => $row["eventID"],
+              'color' => $color,
+              'title'   => $row["eventName"],
+              'start'   => $check_date. " ".$row["timeFrom"],
+              'end'   => $check_date. " ".$row["timeTo"],
+              'facility' => $row["facilityName"]
+            );
+        }
+    }
+    
+        /*
+     $data[] = array(
+      'id'   => $row["eventID"],
+      'color' => $color,
+      'title'   => $row["eventName"],
+      'start'   => $row["dateFrom"]." ".$row["timeFrom"],
+      'end'   => $row["dateTo"]." ".$row["timeTo"],
+      'facility' => $row["facilityName"]
+      
+     );
      
- }
-    
-//If its a examination
-if ($row["eventName"] == "Exercise Class"){
-     $color = '#63f9b1';
- }
-    
-if ($row["eventName"] == "Non-Exercise Class"){
-    $color = '#f5ff72';
-}
- 
- $data[] = array(
-  'id'   => $row["eventID"],
-  'color' => $color,
-  'title'   => $row["eventName"],
-  'start'   => $row["dateFrom"]." ".$row["timeFrom"],
-  'end'   => $row["dateTo"]." ".$row["timeTo"],
-  'facility' => $row["facilityName"]
- );
+     */
 }
 
 
