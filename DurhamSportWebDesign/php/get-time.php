1a474a71ffa08bc9dba5eecf81ea806794a01538
CHENGLONG ZHENG <50271694+Corwin00@users.noreply.github.com>
2019-05-22 17:50:41 +0100

    debug

--- DurhamSportWebDesign/php/get-time.php
+++ DurhamSportWebDesign/php/get-time.php
@@ -13,6 +13,8 @@ $pdo = make_database_connection();
 $facilityId = $_POST['facilityId'];
 $date = $_POST['date'];
 
+$date1 = strtotime($date);
+
 $sql = "select * from booking where facilityID = '$facilityId' AND bookingDate = '$date'";
 $result = $pdo->query($sql);
 $rows = $result->fetchAll(PDO::FETCH_ASSOC);
@@ -21,11 +23,18 @@ $sql1 = "select * from facility where facilityID = '$facilityId'";
 $result1 = $pdo->query($sql1);
 $rows1 = $result1->fetch(PDO::FETCH_ASSOC);
 
+//block-booking
+$sql3 = "select * from block_booking";
+$result3 = $pdo->query($sql3);
+$rows3 = $result3->fetchAll(PDO::FETCH_ASSOC);
+$dow = date('w', $date1);
+
 $openTime = $rows1['timeOpen'];
 $closeTime = $rows1['timeClose'];
 $capacity = $rows1['capacity'];
 
-$duration = array();  
+$duration = array();
+$count = 0;
 
 foreach($rows as $row){
     $time = $row['startTime'];
@@ -44,12 +53,35 @@ echo "<label for='select'>Time</label>
 
 for($i=$openTimeNum;$i<$closeTimeNum;$i++){
     $str = $i.":00:00";
+
+    $startTime = $i."0000";
+    $endTime = ($i+1)."0000";
+    $flag = true;
+
+    foreach($rows3 as $row){    
+        if ($facilityId == $row["facilityID"] && (strpos($row['dow'], $dow) !== FALSE)) {	
+
+            $timeFrom = str_replace(":", "", $row["timeFrom"]);
+            $timeTo = str_replace(":", "", $row["timeTo"]);
+    
+            $dateFrom = strtotime($row["dateFrom"]);
+            $dateTo = strtotime($row["dateTo"]);
+    
+            if ($date1 >= $dateFrom && $date1 <= $dateTo) {
+                if (($startTime > $timeFrom && $startTime < $timeTo)
+                    || ($endTime > $timeFrom && $endTime < $timeTo)){                        
+                        $flag = false;
+                }
+            }
+        }       
+    }
+
     if(!array_key_exists($str, $duration) || $duration[$str]<$capacity){
-        $j = $i+1;
-        echo "<option value= '$i'>$i:00-$j:00</option>";
-     }
+        if($flag){
+            $j = $i+1;
+            echo "<option value= '$i'>$i:00-$j:00</option>";
+        }
+    }        
 }
-
 echo "</select>";
-
 ?>
